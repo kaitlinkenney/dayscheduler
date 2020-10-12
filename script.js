0740c218162ae39d80204f73a4c2d0d3ba4dee74
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-12 15:44:39 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var $timeBlock = $(".time-block");
+var row = $(".row");
+var hour = $(".hour");
+var past = $(".past");
+var present = $(".present");
+var future = $(".future");
+var saveBtn = $(".saveBtn");
+
+$(".saveBtn").on("click", function(){
+    console.log("I have clicked the button");
+    $(past.val
+    localStorage
+})
