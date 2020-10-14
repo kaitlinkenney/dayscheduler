0f0876de38245371fbd15fdc5b00b2131dd8cb16
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-14 12:39:08 -0400

    third commit

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 var $timeBlock = $(".time-block");
 var row = $(".row");
-var hour = $(".hour");
-var past = $(".past");
-var present = $(".present");
-var future = $(".future");
+var blockHour = $(".hour");
+var textArea = $(".description");
 var saveBtn = $(".saveBtn");
+
+//if current hour is greater than the block hour then do this-->
+var currentHour = moment.utc([2011, 0, 1, 8]);
+a.hours(); // 8 UTC
+a.local();
+a.hours(); // 0 PST
+
+if (currentHour > blockHour) {
+    $(this).addClass("past");
+}
+else if (currentHour < blockHour) {
+   // remove class past
+    Element.past.remove('hidden');
+    $(this).addClass("future");
+}
+else (currentHour === blockHour) {
+    Element.past.remove('hidden');
+    Element.future.remove('hidden');
+    $(this).addClass("present");
+}
+
 
 $(".saveBtn").on("click", function(){
     console.log("I have clicked the button");
-    $(past.val
-    localStorage
+    $(textArea.val());
+    localStorage.setItem("texArea", JSON.stringify("textArea"));
 })
+
+var textArea = JSON.parse(localStorage.getItem("textArea"));
+var finalText = textArea[textArea.length - 1];
