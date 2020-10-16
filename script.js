71540c3965e428d4657aea6e17437114ebd7d884
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-16 16:18:59 -0400

    sixth commit

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 var currentDate = moment().format("LLLL");
     console.log(currentDate);
+    // $("#currentDay").append(currentDate);
+
+var saveBtn = $(".saveBtn");
 
 $(".saveBtn").on("click", function(){
     console.log("I have clicked the button");
     //grab the value, and the time, set both to separate variables and tell it to grab
-    $(this).val
+    var thisVal = $(this).attr(".description");
+    var thisBlock = $(this).attr(".time-block");
+    localStorage.setItem(thisVal, thisBlock);
     //the information (this) grab description in one variable and time in another, and set
 //     to local storage
-})
+});
+
+
 
 
 function hourUpdater(){
@@ -20,29 +27,16 @@ function hourUpdater(){
             $(this).addClass("past");
         }
         else if (currentHour === blockHour) {
-           // remove class past
-            $(this).removeClass('past');
+            $(this).removeClass("past");
             $(this).addClass("present");
         }
         else {
-            $(this).removeClass('past');
-            $(this).removeClass('present');
+            $(this).removeClass("past");
+            $(this).removeClass("present");
             $(this).addClass("future");
         }
     })
 }
 hourUpdater();
 
-console.log("Javascript is functioning correctly!!")
-
-
-
-
-
-$(".description").val().trim(),
-
-
-localStorage.setItem("textareaInput", JSON.stringify("textareaInput"));
-
-var textArea = JSON.parse(localStorage.getItem("textareaInput"));
-// var finalText = textArea[textareaInput.length - 1];
+$(".description").val().trim();
