f6f8c698a38bd13541173e6d50ac073e1b2ad3ce
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-15 21:32:33 -0400

    fourth commit

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-var $timeBlock = $(".time-block");
-var row = $(".row");
-var blockHour = $(".hour");
-var textArea = $(".description");
-var saveBtn = $(".saveBtn");
-
-//if current hour is greater than the block hour then do this-->
-var currentHour = moment.utc([2011, 0, 1, 8]);
-a.hours(); // 8 UTC
-a.local();
-a.hours(); // 0 PST
-
-if (currentHour > blockHour) {
-    $(this).addClass("past");
-}
-else if (currentHour < blockHour) {
-   // remove class past
-    Element.past.remove('hidden');
-    $(this).addClass("future");
-}
-else (currentHour === blockHour) {
-    Element.past.remove('hidden');
-    Element.future.remove('hidden');
-    $(this).addClass("present");
-}
-
-
 $(".saveBtn").on("click", function(){
     console.log("I have clicked the button");
-    $(textArea.val());
-    localStorage.setItem("texArea", JSON.stringify("textArea"));
+    //grab the value, and the time, set both to separate variables and tell it to grab
+    //the information (this) grab description in one variable and time in another, and set
+//     to local storage
 })
 
-var textArea = JSON.parse(localStorage.getItem("textArea"));
-var finalText = textArea[textArea.length - 1];
+
+//if current hour is greater than the block hour then do this-->
+// var currentHour = moment.utc([2011, 0, 1, 8]);
+// a.hours(); // 8 UTC
+// a.local();
+// a.hours(); // 0 PST
+function hourUpdater(){
+    var currentHour = moment().hours();
+    //loop over timeblocks\
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+        if (currentHour > blockHour) {
+            $(this).addClass("past");
+        }
+        else if (currentHour === blockHour) {
+           // remove class past
+            $(this).removeClass('past');
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass("future");
+        }
+    })
+}
+console.log("Javascript is functioning correctly!!")
+
+
+
+$(".description").val().trim(),
+
+
+localStorage.setItem("textareaInput", JSON.stringify("textareaInput"));
+
+var textArea = JSON.parse(localStorage.getItem("textareaInput"));
+// var finalText = textArea[textareaInput.length - 1];
