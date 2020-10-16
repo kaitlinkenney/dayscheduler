868f86979b83357b5f3e498fe93b943cbde0e599
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-16 13:00:44 -0400

    fifth commit

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+var currentDate = moment().format("LLLL");
+    console.log(currentDate);
+
 $(".saveBtn").on("click", function(){
     console.log("I have clicked the button");
     //grab the value, and the time, set both to separate variables and tell it to grab
+    $(this).val
     //the information (this) grab description in one variable and time in another, and set
 //     to local storage
 })
 
 
-//if current hour is greater than the block hour then do this-->
-// var currentHour = moment.utc([2011, 0, 1, 8]);
-// a.hours(); // 8 UTC
-// a.local();
-// a.hours(); // 0 PST
 function hourUpdater(){
     var currentHour = moment().hours();
-    //loop over timeblocks\
+    //loop over timeblocks
     $(".time-block").each(function() {
         var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
@@ -32,7 +31,11 @@ function hourUpdater(){
         }
     })
 }
+hourUpdater();
+
 console.log("Javascript is functioning correctly!!")
+
+
 
 
 
