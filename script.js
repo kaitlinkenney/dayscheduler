3e31d0264c84e0d8c8ae0a1a29ba528cdb615ffe
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-17 10:02:07 -0400

    eighth commit

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ var currentDate = moment().format("LLLL");
 $(".saveBtn").on("click", function(){
     console.log("I have clicked the button");
     //grab the value, and the time, set both to separate variables and tell it to grab
-    var thisVal = $(this).attr(".description");
-    var thisBlock = $(this).attr(".time-block");
+    var thisVal = $(this).siblings(".description").val();
+    var thisBlock = $(this).parent().attr("id");
     localStorage.setItem(thisVal, JSON.stringify("thisVal"));
     localstorage.setItem(thisBlock, JSON.stringify("thisBlock"));
+        // console.log("thisBlock", JSON.stringify("thisBlock"));
     //the information (this) grab description in one variable and time in another, and set
 //     to local storage
 });
+
+
+// $(".description").val(localStore.getItem("description"));(localStorage.getItem(thisVal, JSON.parse("thisVal"));
+// localStorage.getItem(thisBlock, JSON.parse("thisBlock"));
 
 
 function hourUpdater(){
@@ -34,6 +39,10 @@ function hourUpdater(){
         }
     })
 }
+
+$("#hour-9 .description").val(localStorage.getItem("#hour-9"));
+
+
 hourUpdater();
 
 $(".description").val().trim();
